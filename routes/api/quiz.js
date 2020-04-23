000c9706e3546974c0b63a552a5fb45a9e545e82
Shannon Scirghio <shanscirg7@gmail.com>
2020-04-23 00:03:25 -0700

    modifed quiz route

--- routes/api/quiz.js
+++ routes/api/quiz.js
@@ -4,11 +4,5 @@ const quizController = require("../../controllers/quizController");
 // Matches with "/api/quiz"
 router.route("/")
   .get(quizController.findAll)
-  .post(quizController.create);
-
-// Matches with "/api/quiz/:id"
-router
-  .route("/:id")
-  .get(quizController.findById)
 
 module.exports = router;
