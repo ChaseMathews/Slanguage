8b6f1476ddcde805aa75ec8b7c0399125d33a3a8
Chase Mathews <cha2169540@maricopa.edu>
2020-04-23 20:20:59 -0700

    Updated routes, updated home page components and stored them in a home page view, updated sign up components and made the buttons functional

---
+++ client/src/components/HomePage/HomeView.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import Submit from './submitButton';
+import Image from './image';
+import SignInForm from './SignInForm';
+import SignUpBtn from './signUpButton';
+
+import { Link } from 'react-router-dom';
+
+export default function HomeView() {
+
+
+    return (
+        <Container>
+            <hr></hr>
+            <Row>
+                <Col className="image-container" md={{ span: 8}}>
+                    <Image />
+                </Col>
+                 
+                    <Col className="form-container" md={{ span: 4, offset: 8 }}>
+                        <SignInForm />
+                        <Submit />
+                        <br></br>
+                        <Link to='/UserSignUp'><SignUpBtn /></Link>
+
+                    </Col>
+                
+            </Row>
+        </Container>
+    );
+}
---
+++ client/src/components/HomePage/submitButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+
+
+export default function Submit() {
+    return (
+        <Button variant="primary" size="lg" block>
+    Submit
+  </Button>
+    )
+}
