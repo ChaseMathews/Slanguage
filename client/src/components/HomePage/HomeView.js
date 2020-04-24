7e2ae89b06d022f7914f7bb3087b39ae4e833ba0
Shannon Scirghio <shanscirg7@gmail.com>
2020-04-24 15:22:56 -0700

    edited formatting

--- client/src/components/HomePage/HomeView.js
+++ client/src/components/HomePage/HomeView.js
@@ -4,28 +4,23 @@ import Submit from './submitButton';
 import Image from './image';
 import SignInForm from './SignInForm';
 import SignUpBtn from './signUpButton';
-
 import { Link } from 'react-router-dom';
 
 export default function HomeView() {
-
 
     return (
         <Container>
             <hr></hr>
             <Row>
-                <Col className="image-container" md={{ span: 8}}>
+                <Col className="image-container" md={{ span: 8 }}>
                     <Image />
                 </Col>
-                 
-                    <Col className="form-container" md={{ span: 4, offset: 8 }}>
-                        <SignInForm />
-                        <Submit />
-                        <br></br>
-                        <Link to='/UserSignUp'><SignUpBtn /></Link>
-
-                    </Col>
-                
+                <Col className="form-container" md={{ span: 4, offset: 8 }}>
+                    <SignInForm />
+                    <Submit />
+                    <br></br>
+                    <Link to='/UserSignUp'><SignUpBtn /></Link>
+                </Col>
             </Row>
         </Container>
     );
