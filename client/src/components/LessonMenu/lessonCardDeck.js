02c402662fb703bc019a5d0b5f9b1b0a9946a83b
Chase Mathews <cha2169540@maricopa.edu>
2020-04-25 12:48:09 -0700

    seeds

--- client/src/components/LessonMenu/lessonCardDeck.js
+++ client/src/components/LessonMenu/lessonCardDeck.js
@@ -1,46 +1,46 @@
-// import React from 'react';
-// import { CardDeck, Card } from 'react-bootstrap';
+import React from 'react';
+import { CardDeck, Card } from 'react-bootstrap';
 
-// export default function LessonCards() {
+export default function LessonCards() {
 
-//     return (
+    return (
 
-//         <CardDeck>
-//             <Card>
-//                 {/* We want to seed the card Image/Title/Info from the database */}
-//                 <Card.Img variant="top" src="" />
-//                 <Card.Body>
-//                     <Card.Title>Card title</Card.Title>
-//                     <Card.Text>
-//                         This card has supporting text below as a natural lead-in to additional content.{' '}
-//                     </Card.Text>
-//                 </Card.Body>
-//             </Card>
+        <CardDeck>
+            <Card>
+                {/* We want to seed the card Image/Title/Info from the database */}
+                <Card.Img variant="top" src="" />
+                <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        This card has supporting text below as a natural lead-in to additional content.{' '}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
 
-//             <Card>
-//                 {/* We want to seed the card Image/Title/Info from the database */}
-//                 <Card.Img variant="top" src="" />
-//                 <Card.Body>
-//                     <Card.Title>Card title</Card.Title>
-//                     <Card.Text>
-//                         This card has supporting text below as a natural lead-in to additionalcontent.{' '}
-//                     </Card.Text>
-//                 </Card.Body>
-//             </Card>
+            <Card>
+                {/* We want to seed the card Image/Title/Info from the database */}
+                <Card.Img variant="top" src="" />
+                <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        This card has supporting text below as a natural lead-in to additionalcontent.{' '}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
 
-//             <Card>
-//                 {/* We want to seed the card Image/Title/Info from the database */}
-//                 <Card.Img variant="top" src="" />
-//                 <Card.Body>
-//                     <Card.Title>Card title</Card.Title>
-//                     <Card.Text>
-//                         This card has supporting text below as a natural lead-in to additional content.{' '}
-//                     </Card.Text>
-//                 </Card.Body>
-//             </Card>
+            <Card>
+                {/* We want to seed the card Image/Title/Info from the database */}
+                <Card.Img variant="top" src="" />
+                <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        This card has supporting text below as a natural lead-in to additional content.{' '}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
 
 
-//         </CardDeck>
+        </CardDeck>
 
-//     )
-// }
+    )
+}
