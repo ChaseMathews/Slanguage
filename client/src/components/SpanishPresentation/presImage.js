9883341a5204841f0f862e414fb938180b99d182
J-Navajo <jnnfr.stnly@gmail.com>
2020-04-26 19:16:29 -0700

    making it look pretty in view page

--- client/src/components/SpanishPresentation/presImage.js
+++ client/src/components/SpanishPresentation/presImage.js
@@ -1,2 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
+import API from "../../utils/API"
 
+
+// function PresImage() {
+//     const [image, setImage] = useState([])
+//     // Load image
+//     useEffect(() => {
+//         imageHolder()
+//     }, [])
+    
+//     function imageHolder() {
+//         API.getPresData()
+//           .then(res => 
+//             console.log(),
+//             setImage(res.data)
+            
+//           )
+//           .catch(err => console.log(err));
+//       };
+    
+// }
+// export default PresImage
+// function presData() {
+
+// }
+
+class PresImage extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        isFetchingData: false,
+        data: null
+      };
+    }
+    componentDidMount () {
+      this.setState({ isFetchingData: true });
+      API.getPresData().then((data) => {
+          console.log(data)
+        this.setState({
+          isFetchingData: true,
+          data
+        });
+      });
+    }
+    render () {
+      if (!this.state.data) {
+        return <img src="https://lh4.googleusercontent.com/JGHNuGPjr0Xnxwpvnv0h3tL0YgtllY88PNSeGhZ_lQI2RJhiKBoBehbv-0brtqkpBrwLhti4gkJAAL8d6vwj8nVh_UiqTFN1MIJ075c"></img>;
+      }
+      if (!this.state.isLoading) {
+        return <p>Loading data</p>;
+      }
+      // Render your component with data
+    }
+  }
+
+  export default PresImage
--- controllers/presController.js
+++ controllers/presController.js
@@ -6,7 +6,9 @@ module.exports = {
   findAll: (req, res) => {
     db.Presentation
       .find(req.query)
-      .then(dbModel => res.json(dbModel))
+      .then(dbModel => {
+        console.log(dbModel);
+          res.json(dbModel)}) 
       .catch(err => res.status(422).json(err));
   }
 };
