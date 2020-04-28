a5daf7bc073f877d2bd8e9112c3efe6864173002
Shannon Scirghio <shanscirg7@gmail.com>
2020-04-27 19:27:47 -0700

    added useraudio and playaudio components to spapresview, converted spanish numbers audios to mp3 in seeds file

--- client/src/components/SpanishPresentation/spaPresView.js
+++ client/src/components/SpanishPresentation/spaPresView.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Container, Jumbotron, Card, Button} from 'react-bootstrap';
+import { Container, Jumbotron, Card, Button } from 'react-bootstrap';
 import SpaPresQuizBtn from './spaPresQuizBtn';
-import PresImage from "./presImage"
-import "./style.css"
+import PresImage from "./presImage";
+import "./style.css";
+import UserAudio from "./userAudio";
+import PlayAudio from "./playAudio";
 
 
 export default function SpanishPresentation() {
@@ -19,13 +21,11 @@ export default function SpanishPresentation() {
 
                 <Card className="cardHolder" style={{ width: '18rem' }}>
                     <PresImage />
-                    {/* <Card.Img variant="top" src="https://p7.hiclipart.com/preview/994/690/368/loudspeaker-computer-icons-sound-icon-call-icon.jpg" /> */}
+                    <Card.Img variant="top" id="audioIcon" src="https://p7.hiclipart.com/preview/994/690/368/loudspeaker-computer-icons-sound-icon-call-icon.jpg" />
                     <Card.Body>
                         <Card.Title>Uno</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-    </Card.Text>
+                        <PlayAudio url="https://cf-media.sndcdn.com/1JCfsLlYB6oM.128.mp3?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiKjovL2NmLW1lZGlhLnNuZGNkbi5jb20vMUpDZnNMbFlCNm9NLjEyOC5tcDMiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE1ODgwNDAzNzl9fX1dfQ__&Signature=CFZP7~0IND0EQtzk09znAoftiot8ILJkx9CdMzK90SVc3oPweWXuKf5BetOqXriu6N-aWfFhBQUhfIZQ0vFHrOyS473y7lomltELOTExUm9seDgx1TqMTjCVYhijJZcE3sYDt5wDUgkHdPi7oQ-ukGDMaqhPnNgLnXe0vyt5BZQGup2WiGkP388niDmEKJut9OBxiPdpAD8aOANFtmYF-hE8E~AQKhHZzj4-aA1FWrQ5qsFCTTkY7Lk9HbaJCAAxGOmZqd-ZMIzkXBgMnLeazezU0D8otSd03CLki4td7gVdpT2FeS-EcWaKGyNocwgyQfXPX2VsoiGUFXmA~N0UeQ__&Key-Pair-Id=APKAI6TU7MMXM5DG6EPQ" />
+                        <UserAudio />
                         <Button variant="primary">Go to next</Button>
                     </Card.Body>
                 </Card>
