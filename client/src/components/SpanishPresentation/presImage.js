883e1b0a282418bce45ae1d90dfe97327d011a99
Shannon Scirghio <shanscirg7@gmail.com>
2020-04-28 07:15:29 -0700

    added a back button

--- client/src/components/SpanishPresentation/presImage.js
+++ client/src/components/SpanishPresentation/presImage.js
@@ -24,11 +24,15 @@ function PresImage() {
   const handleImageChange = () => {
     setIndex(prev => prev + 1)
   }
+  const handleImageChangeBack = () => {
+    setIndex(prev => prev - 1)
+  }
 
   return (
     <>
       {image &&
         <div>
+          <button onClick={handleImageChangeBack}>Back</button>
           <button onClick={handleImageChange}>Click</button>
           <img id="numberImage" src={image[index].imageUrl}></img>
         </div>
