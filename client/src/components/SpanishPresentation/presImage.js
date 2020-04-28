429c3e7dacdc9f6f0105315eb88f4c5fb25be9ca
Shannon Scirghio <shanscirg7@gmail.com>
2020-04-27 21:47:08 -0700

    fixed numberimages in seeds again, working on presimage and spapresview

--- client/src/components/SpanishPresentation/presImage.js
+++ client/src/components/SpanishPresentation/presImage.js
@@ -1,39 +1,50 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import API from "../../utils/API"
 import "./style.css"
 
 function PresImage() {
 
-  const [image, setImage] = useState()
-  const [index, setIndex] = useState(0);
+  const [image, setImage] = useState("")
+  // const [index, setIndex] = useState(0);
 
   useEffect(() => {
+    async function loadImages(){
+      API.getPresData()
+        .then(res => {
+          let imageUrl;
+          for (const img in res.data[0].content) {
+            imageUrl = res.data[0].content[img].imageUrl;
+          }
+          // console.log(imageUrl);
+          setImage(imageUrl)
+        })
+        .catch (err => console.log(err));
+    }
     loadImages()
   }, [])
-  function loadImages() {
-    API.getPresData()
-      .then(res => {
-        setImage(res.data)
-        console.log(res.data)
-      })
 
-      .catch(err => console.log(err));
-  };
+  useEffect(() => {
+    console.log('rendering images', image);
+  }, [image]);
 
-  // const handleImageChange = () => {
 
-  // }
+// const nextImageHandler = () => {
+//   this.setImage()
+// }
 
-  return (
-    <>
+
+
+return (
+  <>
     {console.log(image)}
-    {image &&   
-    <div>
-        <img id="numberImage" src={image[0].content[index].imageUrl}></img>
-    </div> 
+    {image &&
+      <div>
+        {/* <img id="numberImage" src={image[0].content[index].imageUrl} alt="number-image"></img> */}
+        <img id="numberImage" src={image} alt="number-image"></img>
+      </div>
     }
-    </>
-  )
+  </>
+)
 }
 
-export default PresImage
+export default PresImage;
