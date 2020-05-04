decdf2e134695b350a015af66a09277b1d5e23ce
J-Navajo <jnnfr.stnly@gmail.com>
2020-05-04 00:17:02 -0700

    added styling to the jumbotron and the effects to the cards

--- client/src/components/LessonMenu/LessonMenuView.js
+++ client/src/components/LessonMenu/LessonMenuView.js
@@ -2,20 +2,29 @@ import React from 'react';
 import { Container, Jumbotron, Row, Col } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import LessonCards from './lessonCardDeck';
+import { slideInDown } from 'react-animations';
+import styled, { keyframes } from 'styled-components';
+import "./style.css"
+import Gradient from "../Gradient"
+const SlideDown = styled.div`animation: 2s ${keyframes`${slideInDown}`} 1`;
 
 
-function LessonMenu () {
+function LessonMenu() {
     const { lang } = useParams();
 
     return (
-<Container>
-            <Jumbotron>
-                <Row><h1>{lang}</h1></Row>
-                <Row>
-                    <Col>
-                    <LessonCards />
-                    </Col>
-                </Row>
+        <Container>
+            <Jumbotron className="tron">
+                <Gradient />
+                    <SlideDown>
+                        <Row><h1>{lang}</h1></Row>
+                        <Row>
+                            <Col>
+                                <LessonCards />
+                            </Col>
+                        </Row>
+                    </SlideDown>
+                {/* </Gradient> */}
             </Jumbotron>
         </Container>
     )
