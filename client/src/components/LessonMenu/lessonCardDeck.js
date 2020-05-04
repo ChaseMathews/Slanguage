b079ca86da8fdbe043a6315adb1ac9cbce2d1018
J-Navajo <jnnfr.stnly@gmail.com>
2020-05-04 01:54:01 -0700

    styling the cards

--- client/src/components/LessonMenu/lessonCardDeck.js
+++ client/src/components/LessonMenu/lessonCardDeck.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { useParams, Link } from "react-router-dom";
-import { CardDeck, Card, Button } from 'react-bootstrap';
-import { slideInDown } from 'react-animations';
-import styled, { keyframes }  from 'styled-components';
-
-const SlideDown = styled.div`animation: 2s ${keyframes`${slideInDown}`} 1`;
+import { CardDeck, Card, Button, Row } from 'react-bootstrap';
+import LangCategory from "./lessonLangHeader"
 
 
 export default function LessonCards() {
@@ -12,49 +9,50 @@ export default function LessonCards() {
     const url = lang === "Spanish" ? "/SpanishPresentation/" : "/NavajoPresentation/";
 
     return (
+        
 
         <CardDeck>
-            <SlideDown>
+            <Row>
+            <LangCategory />
+            </Row>
+            <Row>
             <Card>
                 {/* We want to seed the card Image/Title/Info from the database */}
-                <Card.Img variant="top" src="" />
+                <Card.Img variant="top" src="https://i.pinimg.com/originals/4c/ea/e7/4ceae7ca22156bb1996f384a34afeab9.gif" />
                 <Card.Body>
                     <Card.Title>Numbers</Card.Title>
-                    <Card.Text>
+                    <Card.Text className="card-text">
                         Learn numbers 1 - 10!{' '}
                     </Card.Text>
                     <Link to={url + "numbers"}><Button>Click Here!</Button></Link>
                 </Card.Body>
             </Card>
-            </SlideDown>
+            
 
-            <SlideDown>
             <Card>
-                {/* We want to seed the card Image/Title/Info from the database */}
+                
                 <Card.Img variant="top" src="" />
                 <Card.Body>
                     <Card.Title>Slang</Card.Title>
                     <Card.Text>
                         Learn some slang words and phrases!{' '}
                     </Card.Text>
-                    <Link to={url + "slang"}><Button disabled>Click Here!</Button></Link>
+                    {/* <Link to={url + "slang"}><Button disabled>Click Here!</Button></Link> */}
                 </Card.Body>
             </Card>
-            </SlideDown>
 
-            <SlideDown>
             <Card>
-                {/* We want to seed the card Image/Title/Info from the database */}
+                
                 <Card.Img variant="top" src="" />
                 <Card.Body>
                     <Card.Title>Basic Travel Phrases</Card.Title>
                     <Card.Text>
                         Going on a trip? Learn some basic travel vocabulary!{' '}
                     </Card.Text>
-                    <Link to={url + "travel"}><Button disabled>Click Here!</Button></Link>
+                    {/* <Link to={url + "travel"}><Button disabled>Click Here!</Button></Link> */}
                 </Card.Body>
             </Card>
-            </SlideDown>
+            </Row>
 
         </CardDeck>
 
