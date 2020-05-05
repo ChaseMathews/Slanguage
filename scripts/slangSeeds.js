8da07e8a1caf65398554535b8574f6d35cea952c
Karen Montes <karen.montes04@gmail.com>
2020-05-04 19:48:34 -0700

    started seeds for spanish slang presentation

---
+++ scripts/slangSeeds.js
@@ -0,0 +1,70 @@
+module.exports= [
+    [
+        {
+            "language": "Spanish",
+            "content": [
+                {
+                    "imageUrl": "",
+                    "targetWord": "Güey",
+                    "audioToPlay": "",
+                    "phonetic": ""
+                },
+                {
+                    "imageUrl": "https://cdn.pixabay.com/photo/2019/07/07/07/02/trust-4321881__340.jpg",
+                    "targetWord": "Neta",
+                    "audioToPlay": "",
+                    "phonetic": ""
+                },
+                {
+                    "imageUrl": "https://cdn.pixabay.com/photo/2017/02/11/19/24/headache-2058476__340.png",
+                    "targetWord": "Crudo",
+                    "audioToPlay": "",
+                    "phonetic": ""
+                },
+                {
+                    "imageUrl": "",
+                    "targetWord": "¿Qué onda?",
+                    "audioToPlay": "",
+                    "phonetic": ""
+                },
+                {
+                    "imageUrl": "https://cdn.pixabay.com/photo/2014/08/20/15/35/thumb-422558__340.jpg",
+                    "targetWord": "Órale",
+                    "audioToPlay": "",
+                    "phonetic": ""
+                },
+                {
+                    "imageUrl": "",
+                    "targetWord": "¿Que pedo?",
+                    "audioToPlay": "",
+                    "phonetic": ""
+                },
+                {
+                    "imageUrl": "",
+                    "targetWord": "Chido",
+                    "audioToPlay": "",
+                    "phonetic": ""
+                },
+                {
+                    "imageUrl": "",
+                    "targetWord": "Gacho",
+                    "audioToPlay": "",
+                    "phonetic": ""
+                },
+                {
+                    "imageUrl": "https://cdn.pixabay.com/photo/2016/09/14/11/35/beer-1669273__340.png",
+                    "targetWord": "Cheve/Chela",
+                    "audioToPlay": "",
+                    "phonetic": ""
+                },
+                {
+                    "imageUrl": "https://cdn.pixabay.com/photo/2013/07/12/16/00/error-150717__340.png",
+                    "targetWord": "Aguas!",
+                    "audioToPlay": "",
+                    "phonetic": ""
+                }
+
+            ]
+        }
+    ]
+]
