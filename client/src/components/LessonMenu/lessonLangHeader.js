191028f4f19d34c951b2464f3327dee1dae223b7
J-Navajo <jnnfr.stnly@gmail.com>
2020-05-04 20:05:33 -0700

    update styling

--- client/src/components/LessonMenu/lessonLangHeader.js
+++ client/src/components/LessonMenu/lessonLangHeader.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useParams, Link } from 'react-router-dom';
-import { Row } from 'react-bootstrap';
+import "./style.css";
 
 export default function LangCategory() {
     const { lang } = useParams();
     const url = lang === "Spanish" ? "/SpanishPresentation/" : "/NavajoPresentation/";
     return (
-        <><h1>{lang}</h1></>
+        <>
+        <h1 className="lang">{lang}</h1>
+        </>
     );
 }
