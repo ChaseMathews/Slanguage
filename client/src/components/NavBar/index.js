07768430f81b430d34a0694784284598a6e73728
J-Navajo <jnnfr.stnly@gmail.com>
2020-05-06 03:29:20 -0700

    added logo to navbar

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -6,7 +6,13 @@ import Link from 'react-router-dom'
 export default function NavBar() {
   return (  
 <Navbar bg="dark" variant="dark">
-    <Navbar.Brand className="text" href="/">Slanguage</Navbar.Brand>
+    <Navbar.Brand className="text" href="/">
+      <img src="https://raw.githubusercontent.com/J-Navajo/Updated-Portfolio/master/assets/images/slanguagelogoFinal-02.png"
+      width="120"
+      height="40"
+      alt="Slanguage logo"
+      />
+      </Navbar.Brand>
     <Nav className="mr-auto">
       <Nav.Link href="link-1">My Dashboard</Nav.Link>
       <Nav.Link href="link-1">Account Settings</Nav.Link>
