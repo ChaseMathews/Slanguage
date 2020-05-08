69f47bbff25d7e55bb542434bcdcdd93c4e3b38e
Karen Montes <karen.montes04@gmail.com>
2020-05-07 21:19:55 -0700

    color on button quiz and conditional render of slang and numbers quiz

--- client/src/components/QuizCard/index.js
+++ client/src/components/QuizCard/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect } from "react";
 import API from "../../utils/API"
 import { Card, Button, Container, Row, Col } from 'react-bootstrap';
 import { useParams, useHistory } from 'react-router-dom';
@@ -13,13 +13,18 @@ export default function QuizCard() {
     ButtonTwo: true,
     ButtonThree: true
   });
-  // const [display, setDisplay] =useState(false)
+  
   const [score, setScore] = useState(0)
   const [display, setDisplay] = useState(false)
-
   const history = useHistory();
-  const { lang } = useParams();
-  const { lesson } = useParams();
+  const { lang, lesson } = useParams();
+  const [btnVarient, setBtnVarient] = useState({
+    button_1: "primary",
+    button_2: "primary",
+    button_3: "primary"
+
+  })
+  
 
   useEffect(() => {
     lesson === "numbers" ? loadNumQuiz() : loadSlangQuiz();
@@ -57,29 +62,34 @@ export default function QuizCard() {
     console.log(e.target.value)
 
     const { value } = e.target
-
-
-    console.log(quizContent)
+    const {name} = e.target
+    console.log(name)
     console.log(quizContent[index].answerOptions)
 
 
+      if (quizContent[index].correctAnswer === value) {
 
+      setBtnVarient({
+        ...btnVarient,
+          [name]: "success"
 
-    if (quizContent[index].correctAnswer === value) {
-
-      setDisplay(true)
+      })
       setScore(score + 3)
 
 
     } else {
+      setBtnVarient({
+        ...btnVarient,
+        [name]: "danger"
+
+      })
 
       setScore(score - 1)
 
-
-
     }
-    console.log(score)
 
+
+    
 
   }
 
@@ -94,7 +104,7 @@ export default function QuizCard() {
   const goToDash = () => {
     history.push(`/Dashboard/${lang}`);
   }
-
+ 
   return (
     <>
       {quizContent &&
@@ -102,20 +112,31 @@ export default function QuizCard() {
           score:{score}
           <Card>
             <Row>
-              <Col sm={4}>
+              <Col sm={lesson !== "numbers" ? 3 : 4}>
                 <Card.Img variant="top" src={quizContent[index].imageUrl} />
               </Col>
-              <Col sm={4}>
-                <Button variant={quizContent[index].correctAnswer === quizContent[index].answerOptions[0] && display ? "success" : "danger"} size="lg" onClick={handleScore} block value={quizContent[index].answerOptions[0]} > {quizContent[index].answerOptions[0]}  </Button>
+              {lesson !== "numbers" ? (
+                  <Col sm={3}>
+                   <div>
+                     What does {quizContent[index].phrase} mean? 
+                   </div>
+                   <br></br>
+                   <div>
+                     Example: {quizContent[index].explanation}
+                   </div>
+                  </Col>
+              ) : ""} 
+              <Col sm={lesson !== "numbers" ? 3 : 4}>
+                <Button variant= {btnVarient.button_1} size="lg" name="button_1" onClick={handleScore} block value={quizContent[index].answerOptions[0]} > {quizContent[index].answerOptions[0]}  </Button>
 
 
-                <Button variant={quizContent[index].correctAnswer === quizContent[index].answerOptions[1] && display ? "success" : "danger"} size="lg" onClick={handleScore} block value={quizContent[index].answerOptions[1]}>{quizContent[index].answerOptions[1]} </Button>
+                <Button variant={btnVarient.button_2} size="lg" name="button_2" onClick={handleScore} block value={quizContent[index].answerOptions[1]}>{quizContent[index].answerOptions[1]} </Button>
 
 
-                <Button variant={quizContent[index].correctAnswer === quizContent[index].answerOptions[2] && display ? "success" : "danger"} size="lg" onClick={handleScore} block value={quizContent[index].answerOptions[2]}> {quizContent[index].answerOptions[2]}</Button>
+                <Button variant={btnVarient.button_3} size="lg" name="button_3" onClick={handleScore} block value={quizContent[index].answerOptions[2]}> {quizContent[index].answerOptions[2]}</Button>
 
               </Col>
-              <Col sm={4}>
+              <Col sm={lesson !== "numbers" ? 3 : 4}>
                 <Row>
                   {index !== 9 ?
                     <Button onClick={handleImageChange} variant="danger" className="">NEXT</Button>
