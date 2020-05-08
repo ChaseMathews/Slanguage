05d450a19defd37590ee8196c4e652555ef96a39
J-Navajo <jnnfr.stnly@gmail.com>
2020-05-08 03:39:48 -0700

    update a path

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -14,7 +14,7 @@ export default function NavBar() {
       />
       </Navbar.Brand>
     <Nav className="mr-auto">
-      <Nav.Link href="/Dashboard/Spanish">My Dashboard</Nav.Link>
+      <Nav.Link href="/DashboardCards">My Dashboard</Nav.Link>
       <Nav.Link href="link-1">Account Settings</Nav.Link>
       <Nav.Link href="/About">About</Nav.Link>
       <Nav.Link href="/">Sign Out</Nav.Link>
