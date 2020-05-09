5ed2d42a60cd668c95a70438e33afab4d2a616f0
Shannon Scirghio <shanscirg7@gmail.com>
2020-05-08 23:11:12 -0700

    removed initial states

--- client/src/utils/Context.js
+++ client/src/utils/Context.js
@@ -4,11 +4,8 @@ export const UserContext = createContext("");
 
 
 export default ({ children }) => {
-    const [user, setUser] = useState({
-        _id: "5eb607b41d152c275d0568cb",
-        username: "shanscirg"
-    });
-    const [currentLang, setCurrentLang] = useState("Spanish");
+    const [user, setUser] = useState();
+    const [currentLang, setCurrentLang] = useState();
 
     return <UserContext.Provider value={{ user, setUser, currentLang, setCurrentLang }}>{children}</UserContext.Provider>
 }
