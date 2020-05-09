2303ae7787c6f508509c60eb0467e0320a2d972a
Shannon Scirghio <shanscirg7@gmail.com>
2020-05-09 01:01:21 -0700

    fixed  seeds, working on account deletion modal, fixed slang quiz button

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -25,7 +25,7 @@ export default function ModalBox() {
           <Modal.Header closeButton>
             <Modal.Title>Modal heading</Modal.Title>
           </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Body></Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
               Close
