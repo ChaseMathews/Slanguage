58c6a5d62abccf41c37a40abca3b1e12c658f502
J-Navajo <jnnfr.stnly@gmail.com>
2020-06-01 21:24:37 -0700

    added a brief intro

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import "./style.css"
 import { Jumbotron, Card, Image, Container, Row, Col } from 'react-bootstrap'
 import { UserContext } from '../../utils/Context';
+import { Link } from 'react-router-dom';
 
 
 
@@ -16,6 +17,7 @@ export default function About() {
             
                 <Card.Body>
                 <h1>About the App</h1>
+                <hr />
                 <p>
                     The team at Slanguage is passionate about learning languages! Our goal was to create a language app that had something most language apps are missing… Slang. Lingo. Colloquialisms. In other words, the expressions you may not find in a dictionary or within the classroom. And for those language learners that do want the basics, Slanguage also offers other awesome categories to learn and practice with!
                 </p>
@@ -25,7 +27,7 @@ export default function About() {
                 </Card.Body>              
                 
                 </Jumbotron>
-                <Card.Body className="team text-center">
+                <Card.Body className="team">
                 <h1 className="team">Meet the Team</h1>
                 <hr />
                 <Row>
@@ -33,7 +35,8 @@ export default function About() {
                         
                             <Image className="profImages" src="https://avatars3.githubusercontent.com/u/57241476?s=460&u=c101a90ddf64df3a7298c4d59b451cabd0908c51&v=4" roundedCircle />
                             <div  className="tooltip1">
-                            <h4 >Karen Sosa</h4><span className="tooltiptext"><h3>Karen</h3><p>a little blurb about Karen.</p></span>
+                            <h4 >Karen Sosa</h4>
+                            <span className="tooltiptext"><h3></h3><p>a little blurb about Karen.</p></span>
                             </div>
                         </Col>
                     
@@ -41,18 +44,25 @@ export default function About() {
 
                             <Image className="profImages" src="https://avatars2.githubusercontent.com/u/56829409?s=460&u=515f9615a0fc438134947916b45c82f774c57e2f&v=4g" roundedCircle />
                             <div  className="tooltip1">
-                            <h4>Shannon Scirghio</h4><span className="tooltiptext"><h3>Shannon</h3><p>a little blurb about Shannon.</p></span>
+                            <h4>Shannon Scirghio</h4>
+                            <span className="tooltiptext"><h3></h3><p>Shannon has a background in teaching foreign languages, so this app hits close to home! She has really enjoyed working with her team, while combining her passions of language education and software development. Check out her other projects at <Link to="github.com/shanscirg">github.com/shanscirg</Link>.</p></span>
                             </div>
                         </Col>
                     
                         <Col>
                             <Image className="profImages" src="https://avatars0.githubusercontent.com/u/57340399?s=460&u=35e107cea016848b8384df6aea8277e4c6a40984&v=4" roundedCircle />
+                            <div  className="tooltip1">
                             <h4>"J" Stanley</h4>
+                            <span className="tooltiptext"><h3></h3><p>A member of the Navajo (Diné) tribe, J has a wealth of knowledge in Navajo language and culture. She enjoys designing and building instructional material as well as delving into programming. You can see her work at <Link to="github.com/j-navajo">github.com/j-navajo</Link>.</p></span>
+                            </div>
                         </Col>
                     
                         <Col>
                             <Image className="profImages" src="https://avatars0.githubusercontent.com/u/56329215?s=460&u=a9594c3cf35b60aa7806c8e38244c3fb0502918b&v=4" roundedCircle />
+                            <div  className="tooltip1">
                             <h4>Chase Mathews</h4>
+                            <span className="tooltiptext"><h3></h3><p>a little blurb about Chase.</p></span>
+                            </div>
                         </Col>
                     </Row>
                     </Card.Body>
