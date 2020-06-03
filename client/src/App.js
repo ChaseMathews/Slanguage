3b0032a1511bae5ea77b38d06cce080463fdf8a4
Karen Montes <karen.montes04@gmail.com>
2020-06-03 16:46:23 -0700

    shannons stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Route, Switch, useLocation } from 'react-router-dom';
+
 import './App.css';
 import HomeView from './components/HomePage/HomeView';
 import SignUp from './components/UserSignUp/SignUpView';
@@ -14,6 +14,8 @@ import CardGradient from './components/CardGradient'
 import About from './components/About'
 import DashboardCard from './components/Dashboard/dashboardCards';
 import API from './utils/API';
+import { Route, Switch, useLocation, Link, Redirect } from 'react-router-dom';
+import {Modal, Button, Image} from 'react-bootstrap';
 
 
 function App() {
@@ -58,6 +60,21 @@ function App() {
           <Route exact path="/CardGradient" component={CardGradient} />
           <Route exact path="/About" component={About} />
           <Route exact path="/DashboardCards/:lang" component={DashboardCard} />
+          <Route exact path="/Premium">
+            {user &&
+              <Modal show={true} backdrop="static" center styles={{ overlay: { background: "#B3F1F8" } }}>
+                <Modal.Header>
+                  <Modal.Body id="modalBody">Oops! Update to Slanguage Premium to take this quiz.</Modal.Body>
+                </Modal.Header>
+                <Image src="https://raw.githubusercontent.com/J-Navajo/Updated-Portfolio/master/assets/images/slangIconPlus.png" styles={{ height: "100px", width: "300px" }} fluid />
+                <Modal.Footer>
+                  <Link to={`/DashboardCards/${user.currentLanguage}`}><Button variant="danger"><strong>Go to Dashboard</strong></Button></Link>
+                  <Button variant="danger" disabled><strong>Update</strong></Button>
+                </Modal.Footer>
+              </Modal>
+            }
+            </Route>
+            <Redirect to="/"></Redirect>
         </Switch>
         </div>
       
